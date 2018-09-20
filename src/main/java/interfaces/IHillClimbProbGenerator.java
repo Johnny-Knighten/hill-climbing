c20c6b78c307b742cadda0506311964bf6b30819
Johnny Knighten <jknigh28@gmail.com>
2018-09-20 14:36:36 -0400

    Added Documentation To Interfaces

--- src/main/java/interfaces/IHillClimbProbGenerator.java
+++ src/main/java/interfaces/IHillClimbProbGenerator.java
@@ -1,5 +1,13 @@
 package interfaces;
 
+/**
+ * Generates random states(IHillClimbProblems) to be used for random restarts in HillClimbRandRestart.
+ */
 public interface IHillClimbProbGenerator {
+    /**
+     *  Generates a random IHillClimbProblem to be used by HillClimbRandRestart
+     *  
+     * @return a random IHillClimbProblem
+     */
     IHillClimbProblem randomState();
 }
